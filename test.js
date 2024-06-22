f00fe3d7dd087c798af0df4645b70551471ad453
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2024-06-22 13:55:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -9,16 +9,27 @@ async function resolveFlipkartUrl(shortenedUrl) {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    
+
     // Navigate to the shortened URL
-    await page.goto(shortenedUrl);
-    
+    await page.goto(shortenedUrl, { waitUntil: 'networkidle2' });
+
     // Extract the final URL after all redirects
     const finalUrl = page.url();
-    
+
     await browser.close();
-    
+
     return finalUrl;
+}
+
+function extractBrandName(url) {
+    const regex = /https:\/\/www\.flipkart\.com\/([^\/]+)/;
+    const match = url.match(regex);
+    if (match) {
+        const brandPart = match[1].split('-')[0];  // Split by '-' and take the first part
+        console.log(brandPart);
+        return brandPart;
+    }
+    return null;
 }
 
 app.get('/resolveShortenedUrl', async (req, res) => {
@@ -29,7 +40,9 @@ app.get('/resolveShortenedUrl', async (req, res) => {
         }
 
         const finalUrl = await resolveFlipkartUrl(url);
-        res.json({ finalUrl });
+        const brandName = extractBrandName(finalUrl);
+        console.log(brandName)
+        res.json({ finalUrl, brandName });
     } catch (error) {
         console.error('Error resolving URL:', error);
         res.status(500).json({ error: 'Internal server error' });
